efdd7db752cf99482b76faf9bedcc8227b4139eb
fromentxavier <fromentxavier@gmail.com>
2021-05-18 22:13:19 +0200

    Updates

---
+++ precache-manifest.7adfba856ccf099c3c7f62256eceb22e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d92caaac9d1c13f1f5",
+    "url": "/matodolist/static/css/main.eb919930.chunk.css"
+  },
+  {
+    "revision": "87d92caaac9d1c13f1f5",
+    "url": "/matodolist/static/js/main.345f2f2e.chunk.js"
+  },
+  {
+    "revision": "98919925d3338b12a9ee",
+    "url": "/matodolist/static/js/runtime~main.0e74331c.js"
+  },
+  {
+    "revision": "b3c40da4dbbafb28ecc8",
+    "url": "/matodolist/static/js/2.d154c491.chunk.js"
+  },
+  {
+    "revision": "9541569e4587519647bcbc559132d28f",
+    "url": "/matodolist/index.html"
+  }
+];
